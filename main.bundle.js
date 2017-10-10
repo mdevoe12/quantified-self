482b45cd116a80d875891a34f2298431155bb47c
Matt DeVoe <matthew.devoe@gmail.com>
2017-10-10 11:53:19 -0600

    10.10 noon deploy

--- main.bundle.js
+++ main.bundle.js
@@ -10495,43 +10495,79 @@
 	const $ = __webpack_require__(2);
 	const api = 'https://lit-caverns-20261.herokuapp.com';
 
-	let consumedCalories = 0;
+	let meals;
 	let goalCalories = 2000;
 
-	function getMeals() {
-	  $.ajax({
-	    method: 'GET',
-	    url: api + '/api/v1/meals',
-	    success: function (data) {
-	      populateMeals(data);
-	    }
-	  });
-	}
+	const goalKey = {
+	  Breakfast: 400,
+	  Lunch: 600,
+	  Dinner: 800,
+	  Snack: 200
+	};
 
-	function populateMeals(data) {
-	  data.forEach(function (meal) {
-	    let mealCalories = 0;
-	    meal.foods.forEach(function (food) {
-	      $(`#${meal.name.toLowerCase()} #foods-header`).after(`<tr class='food-row' data-id=${food.id}>
+	class Meal {
+	  constructor(meal) {
+	    this.name = meal.name;
+	    this.foods = meal.foods;
+	    this.goalCalories = goalKey[this.name];
+	    this.id = meal.id;
+	  }
+	  mealCalories() {
+	    return this.foods.reduce(function (totalCals, food) {
+	      return totalCals + food.calories;
+	    }, 0);
+	  }
+
+	  createTable() {
+	    this.foods.forEach(food => {
+	      $(`#${this.name.toLowerCase()} #foods-header`).after(`<tr class='food-row' data-id=${food.id}>
 	            <td name='name' contenteditable='true'> ${food.name} </td>
 	            <td name='calories' contenteditable='true'> ${food.calories} </td>
 	            <td>
 	              <img class="delete-icon" src="https://raw.githubusercontent.com/mdevoe12/quantified-self/master/images/minus-icon.png"/>
 	            </td>
 	          </tr> `);
-	      mealCalories += food.calories;
 	      shared.focusListener(food.id);
-	      mealIconListener(food.id, meal);
+	      mealIconListener(food.id, this);
 	    });
-	    consumedCalories += mealCalories;
-	    $(`#${meal.name.toLowerCase()} tr`).last().after(`<tr class=meal-${meal.name}>
+
+	    $(`#${this.name.toLowerCase()} tr`).last().after(`<tr class=meal-${this.name}>
 	            <td> Total Calories </td>
-	            <td id="meal-calories"> ${mealCalories} </td>
+	            <td id="meal-calories"> ${this.mealCalories()} </td>
 	           </tr>
-	           <tr class=remaining-${meal.name}>
+	           <tr class=remaining-${this.name}>
 	            <td> Remaining Calories </td>
-	            <td id="remaining-calories"> TBD </td>
+	            <td id="remaining-calories"> ${this.goalCalories - this.mealCalories()} </td>
 	           </tr>`);
+	  }
+	}
+
+	function consumedCalories() {
+	  return meals.reduce(function (totalCals, meal) {
+	    return totalCals + meal.mealCalories();
+	  }, 0);
+	}
+
+	function createMeals(data) {
+	  return data.map(function (meal) {
+	    return new Meal(meal);
+	  });
+	}
+
+	function getMeals() {
+	  $.ajax({
+	    method: 'GET',
+	    url: api + '/api/v1/meals',
+	    success: function (data) {
+	      meals = createMeals(data);
+	      populateMeals(meals);
+	    }
+	  });
+	}
+
+	function populateMeals(meals) {
+	  meals.forEach(function (meal) {
+	    meal.createTable();
 	  });
 	  populateTotals();
 	}
@@ -10543,11 +10579,11 @@
 	     </tr>
 	     <tr>
 	       <td> Calories Consumed </td>
-	       <td> ${consumedCalories} </td>
+	       <td> ${consumedCalories()} </td>
 	     </tr>
 	     <tr>
 	      <td> Remaining Calories </td>
-	      <td> ${goalCalories - consumedCalories} </td>
+	      <td> ${goalCalories - consumedCalories()} </td>
 	     </tr>`);
 	}
 
